0681e36640865bd6d9f4569e9c728b745e70214c
ChemaKing13 <jm2000ho@gmail.com>
2023-07-16 22:49:53 -0600

    models defined

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); 
-const { MongoClient } = require('mongodb'); 
-
-const app = express(); 
-const port = 3001; 
-
-const connectionStringURI = `mongodb://localhost:27017`; 
