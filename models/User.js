473901d0e03ef1187aa316ae4ddbf5a0be6e5d3d
ChemaKing13 <jm2000ho@gmail.com>
2023-07-17 13:13:59 -0600

    user routes working

--- models/User.js
+++ models/User.js
@@ -1,31 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought',
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
     },
-  ],
-});
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
 
 // Define the virtual friendCount
 userSchema.virtual('friendCount').get(function () {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,9 +3,17 @@ const router = require('express').Router();
 const {
     getUsers,
     createUser,
+    getSingleUser,
+    deleteUser,
+    updateUser,
 } = require('../../controllers/userControllers.js'); 
 
 // /api/users
 router.route('/').get(getUsers).post(createUser); 
 
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
 module.exports = router; 
